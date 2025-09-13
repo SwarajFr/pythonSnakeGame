8968ac4aab3d889348bde12b11e2f8745ba38583
swaraj <swarajpanda331@gmail.com>
2025-09-14 03:02:48 +0530

    Update snake class

--- src/snake.py
+++ src/snake.py
@@ -4,28 +4,26 @@ class Snake:
     def __init__(self, cell_size, start_pos):
         self.cell_size = cell_size
         self.segments = [
-            start_pos[:],
-            [start_pos[0] - cell_size, start_pos[1]],
-            [start_pos[0] - 2 * cell_size, start_pos[1]]
+            (start_pos[0], start_pos[1]),
+            (start_pos[0] - cell_size, start_pos[1]),
+            (start_pos[0] - 2 * cell_size, start_pos[1])
         ]
         self.direction = "RIGHT"
 
     def move(self):
-        head = self.segments[0][:]
+        head_x, head_y = self.segments[0]
         if self.direction == "UP":
-            head[1] -= self.cell_size
+            new_head = (head_x, head_y - self.cell_size)
         elif self.direction == "DOWN":
-            head[1] += self.cell_size
+            new_head = (head_x, head_y + self.cell_size)
         elif self.direction == "LEFT":
-            head[0] -= self.cell_size
+            new_head = (head_x - self.cell_size, head_y)
         elif self.direction == "RIGHT":
-            head[0] += self.cell_size
-
-        self.segments.insert(0, head)
-        self.segments.pop()
+            new_head = (head_x + self.cell_size, head_y)
+        self.segments = [new_head] + self.segments[:-1]
 
     def grow(self):
-        self.segments.append(self.segments[-1][:])
+        self.segments.append(self.segments[-1])
 
     def change_direction(self, new_dir):
         opposite = {"UP":"DOWN","DOWN":"UP","LEFT":"RIGHT","RIGHT":"LEFT"}
@@ -33,5 +31,6 @@ class Snake:
             self.direction = new_dir
 
     def draw(self, surface):
-        for seg in self.segments:
-            pygame.draw.rect(surface, (0, 200, 0), (*seg, self.cell_size, self.cell_size))
+        for x, y in self.segments:
+            rect = pygame.Rect(x, y, self.cell_size, self.cell_size)
+            pygame.draw.rect(surface, (0, 200, 0), rect)
