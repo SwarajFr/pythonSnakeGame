9de85b320e4ba8f09d5854534848347666201d4c
swaraj <swarajpanda331@gmail.com>
2025-09-14 02:59:44 +0530

    Update food class

--- src/food.py
+++ src/food.py
@@ -1,16 +1,21 @@
-import pygame, random
+import pygame
+import random
 
 class Food:
     def __init__(self, cell_size, width, height):
         self.cell_size = cell_size
         self.width = width
         self.height = height
-        self.position = self.random_position()
+        self.position = (0, 0)
 
-    def random_position(self):
-        x = random.randint(0, (self.width - self.cell_size) // self.cell_size) * self.cell_size
-        y = random.randint(0, (self.height - self.cell_size) // self.cell_size) * self.cell_size
-        return [x, y]
+    def random_position(self, snake_body, walls):
+        while True:
+            x = random.randrange(0, self.width, self.cell_size)
+            y = random.randrange(0, self.height, self.cell_size)
+            pos = (x, y)
+            if pos not in snake_body and pos not in walls:
+                return pos
 
     def draw(self, surface):
-        pygame.draw.rect(surface, (200, 0, 0), (*self.position, self.cell_size, self.cell_size))
+        rect = pygame.Rect(self.position[0], self.position[1], self.cell_size, self.cell_size)
+        pygame.draw.rect(surface, (200, 0, 0), rect)
